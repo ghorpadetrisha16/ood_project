1cb46118acde829cc2f7a1f08e17cddada9f4d41
eswarsaladi <99567868+eswarsaladi@users.noreply.github.com>
2022-04-13 04:12:10 -0400

    second add db function

--- src/main/java/edu/neu/csye6200/DB.java
+++ src/main/java/edu/neu/csye6200/DB.java
@@ -4,10 +4,24 @@
  */
 package edu.neu.csye6200;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author eswar
  */
 public class DB {
+   static final String DB_URL = "jdbc:mysql://sql209.main-hosting.eu/u625234325_ood_proj";
+   static final String USER = "u625234325_ood_proj";
+   static final String PASS = "Northeastern@4126";
+   
+   public static Connection getConnection() throws SQLException{
+      
+           Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+           return conn;
+       
+   }
     
 }
