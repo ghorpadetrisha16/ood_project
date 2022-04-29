b34950a5109d62af0498530872c3b93cc7e45117
eswarsaladi <99567868+eswarsaladi@users.noreply.github.com>
2022-04-29 14:51:19 -0400

    added query method to DB

--- src/main/java/edu/neu/csye6200/DB.java
+++ src/main/java/edu/neu/csye6200/DB.java
@@ -4,24 +4,70 @@
  */
 package edu.neu.csye6200;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
- *
+ * DB Class - to connect and query from the database.
+ * 
+ * <code> 
+ * db = DB.getObj();                // gives instance of DB
+ * db.query("query-statement");     // gives resultant set
+ * </code>
+ * 
  * @author eswar
  */
 public class DB {
-   static final String DB_URL = "jdbc:mysql://sql209.main-hosting.eu/u625234325_ood_proj";
-   static final String USER = "u625234325_ood_proj";
-   static final String PASS = "Northeastern@4126";
-   
-   public static Connection getConnection() throws SQLException{
-      
-           Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-           return conn;
-       
-   }
-    
+        static final String DB_URL = "jdbc:mysql://sql773.main-hosting.eu/u625234325_ood_proj";
+        static final String USER = "u625234325_ood_proj";
+        static final String PASS = "Northeastern@4126";
+        private Connection conn;
+        private Statement statement;
+
+        public static DB db;
+
+        DB() {
+                createConnection();
+        }
+
+        /**
+         * Connect to database
+         */
+        private void createConnection() {
+                try {
+                        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                        statement = conn.createStatement();
+
+                } catch (SQLException e) {
+                        System.out.print(e.toString());
+                }
+        }
+
+        /**
+         * Query database
+         * 
+         * @param queryStatement
+         * @return
+         */
+        public ResultSet query(String queryStatement) {
+                try {
+                        ResultSet rs = statement.executeQuery(queryStatement);
+                        return rs;
+                } catch (Exception e) {
+                        System.out.print(e.toString());
+                        return null;
+                }
+        }
+
+        /**
+         * Singleton method to get instance
+         * 
+         * @return
+         */
+        public static DB getObj() {
+                if (db == null) {
+                        db = new DB();
+                }
+                return db;
+        }
+
 }
